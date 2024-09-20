d292d4ca92c7f2e54d85d67489c3bd3cde2c5792
meshaljifry <45594879+meshaljifry@users.noreply.github.com>
2024-09-20 12:00:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Orchard House Website Coming soon 
         </a>
       </header>
     </div>
