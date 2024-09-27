c342bc62b624e1777e593eaa3cc08708e52bd4c5
meshaljifry <Aljifrym3769@my.uwstout.edu>
2024-09-27 11:23:15 -0500

    next ui+ tailwind css

---
+++ src/Components/Test.js
@@ -0,0 +1,10 @@
+import {DateInput} from "@nextui-org/react";
+import {CalendarDate} from "@internationalized/date";
+
+export default function Test() {
+  return (
+    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <DateInput label={"Birth date"} placeholderValue={new CalendarDate(1995, 11, 6)} className="max-w-sm" />
+    </div>
+  );
+}
