0767c7544f1ec20ff7e1edd920e5c8be4053441b
meshaljifry <Aljifrym3769@my.uwstout.edu>
2024-09-28 04:37:01 -0500

    more components

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -13,7 +13,7 @@ export default function Settings() {
               <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
               <ModalBody>
                 <p> 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolklkor sit amet, consectetur adipiscing elit.
                   Nullam pulvinar risus non risus hendrerit venenatis.
                   Pellentesque sit amet hendrerit risus, sed porttitor quam.
                 </p>
